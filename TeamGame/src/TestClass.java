030c686e11c10bc8ca1b34b03ccfa6ea965f05fd
ceculbe <ceculbe@ilstu.edu>
2015-02-05 10:22:39 -0600

    Test 3

--- TeamGame/src/TestClass.java
+++ TeamGame/src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 	public static void main(String[] args){
 		
 	System.out.println("This is a commit test");
-	System.out.println("Changed by Culbertson second time");
+	System.out.println("Changed by Culbertson third time");
 	
 	}
 }
